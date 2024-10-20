c659eaa890ed77878c1fc651b67c43dfefe256d5
Pr-Alexis <125223908+Pr-Alexis@users.noreply.github.com>
2024-10-21 00:13:45 +0200

    First operation function draft

--- script/script.js
+++ script/script.js
@@ -3,12 +3,14 @@ const lcd=document.querySelector(".lcd");
 const acButton=document.querySelector("#AC");
 const operators=document.querySelectorAll(".op")
 lcd.textContent="0";
-let firstOperand;
-let secondOperand;
+let firstOperand = 
+    secondOperand =
+    firstOperator =
+    secondOperator = null;
 
 
 function numberConverter(number){
-    //If it finds "." in the converts to float otherwist to integer
+    //If it finds "." in the converts to float otherwise to integer
     if(number.indexOf>-1) return parseFloat(number);
     return parseInt(number);
 }
@@ -24,23 +26,36 @@ function displayClear(){
 }
 
 
-function operatorSwitcher(operation){
-    switch(operation){
-        case "%":
-            console.log("%");
-        break;
-        case "/":
-            console.log("/");
-        break;
-        case "*":
-            console.log(firstOperand);
-        break;
-        case "-":
-            console.log("-");
-        break;
-        case "+":
-            console.log("+");
-        break;
+
+function operate(operation, lcdValue){
+    if(firstOperator==null) {
+        firstOperator=operation;
+        firstOperand= numberConverter(lcdValue);
+        return '';
+    }
+    else {
+        secondOperator = operation;
+        secondOperand = numberConverter(lcdValue);
+    }
+    if(secondOperator.localeCompare('=')==0){
+        switch(firstOperator){
+            case "%":
+                return firstOperand%secondOperand;
+            break;
+            case "/":
+                return firstOperand/secondOperand;
+            break;
+            case "*":
+                return firstOperand*secondOperand;
+            break;
+            case "-":
+                return firstOperand-secondOperand;
+            break;
+            case "+":
+                return firstOperand+secondOperand;
+            break;
+
+        }
     }
 
 }
@@ -55,9 +70,9 @@ buttons.forEach(button=>{
 
 operators.forEach(operator=>{
     operator.addEventListener("click",()=>{
-            firstOperand=numberConverter(lcd.textContent);
-            operatorSwitcher(operator.value);
+            
+            lcd.textContent = operate(operator.value, lcd.textContent);
         })
-}   );
+   });
 
 acButton.addEventListener("click",displayClear);
