6c31c13956621961ec3146c5538d0657995911b4
Pr-Alexis <125223908+Pr-Alexis@users.noreply.github.com>
2024-10-29 01:37:36 +0100

    Fixed operation buttons. Almost feature complete. Still need some work on output.

--- script/script.js
+++ script/script.js
@@ -2,7 +2,9 @@ const buttons= document.querySelectorAll(".num");
 const lcd=document.querySelector(".lcd");
 const acButton=document.querySelector("#AC");
 const operators=document.querySelectorAll(".op")
+const equal=document.querySelector(".equal");
 lcd.textContent="0";
+let displayValue="0";
 let firstOperand = 
     secondOperand =
     firstOperator =
@@ -10,82 +12,123 @@ let firstOperand =
     result = null;
 
 
-function displayUpdater(number){
-    if(lcd.textContent.startsWith("0")) lcd.textContent=number;
-    else lcd.textContent=lcd.textContent+number;
-}
+
 
 function displayClear(){
     lcd.textContent="";
+    displayValue="";
 }
 
+function displayUpdate(value){
+    lcd.textContent=value;
+}
 
-function operate(operation, lcdValue){
-    if(firstOperator==null) {
-        firstOperator=operation;
-        firstOperand= Number(lcdValue);
-        return '';
+function operate(){
+
+    firstOperand=Number(firstOperand);
+    secondOperand=Number(secondOperand);
+
+    switch(firstOperator){
+        case "%":
+            result= firstOperand%secondOperand;
+        break;
+        case "/":
+            result= firstOperand/secondOperand;
+        break;
+        case "*":
+            result= firstOperand*secondOperand;
+        break;
+        case "-":
+            result= firstOperand-secondOperand;
+        break;
+        case "+":
+            result= firstOperand+secondOperand;
+        break;
+    }
+}
+//Change this to handle only operation
+//Add a function to handle =
+//Refactor input handling
+function operationActivation(operator){
+    if(firstOperator == null){
+        firstOperator = operator;
+        firstOperand = displayValue;
+        displayClear();
     }
     else {
-        secondOperator = operation;
-        secondOperand = Number(lcdValue);
+        secondOperator = operator;
+        secondOperand = displayValue;
     }
-    if(secondOperator!==null){
-        switch(firstOperator){
-            case "%":
-                result= firstOperand%secondOperand;
-            break;
-            case "/":
-                result= firstOperand/secondOperand;
-            break;
-            case "*":
-                result= firstOperand*secondOperand;
-            break;
-            case "-":
-                result= firstOperand-secondOperand;
-            break;
-            case "+":
-                result= firstOperand+secondOperand;
-            break;
 
-        }
-       
-        if(secondOperator.localeCompare('=')!==0){
-            firstOperator=secondOperator;
-            firstOperand= result;
-        }
-        else{
-            firstOperator= null;
-            firstOperand= null;
-        }
-
-        secondOperator= null;
-
-        return result.toString();
+    if(firstOperator !== null && secondOperator !== null){
+        operate();
+        displayUpdate(result);
     }
+    if(secondOperator != null && result != null){
+        firstOperand = result;
+        firstOperator = secondOperator;
+        secondOperator = null;
+        secondOperand = null;
+        displayValue = "";
+    }
+    
+}
+
+function equalActivation(){
+    if(firstOperand !== null && firstOperator !== null){
+    secondOperand = displayValue;
+    operate();
+    displayUpdate(result)
+    firstOperand = result;
+    displayValue = result;
+    firstOperator = null;
+    secondOperand = null;
+    result = null;
+    }
+}
+
+
+function inputHandler(operand){
+        if(displayValue =='0') {
+            displayValue = operand;
+        }
+        else if(displayValue == firstOperand){
+            displayValue = operand;
+        }
+        else {
+            displayValue += operand;
+        }
+        
 
 }
 
 
-/*EVENT LISTENER*/
+
+/*EVENT LISTENER*/       v=-
 buttons.forEach(button=>{
     button.addEventListener("click",()=>{
-        if(firstOperator!== null || secondOperator== null && firstOperator== null) displayClear();
-        displayUpdater(button.value);
+            inputHandler(button.value);
+            displayUpdate(displayValue);
+        
         });
     });
 
 operators.forEach(operator=>{
     operator.addEventListener("click",()=>{
-            
-            lcd.textContent = operate(operator.value, lcd.textContent);
+            operationActivation(operator.value);
         })
    });
 
+equal.addEventListener("click",()=>{
+            equalActivation();
+});
+
 acButton.addEventListener("click",()=>{
     displayClear();
-    firstOperand=null;
-    secondOperand=null;
-    firstOperator=null;
-    secondOperator=null;
+    lcd.textContent=0;
+    firstOperand   =
+    secondOperand  =
+    firstOperator  =
+    secondOperator =
+    result         = null;
 });
